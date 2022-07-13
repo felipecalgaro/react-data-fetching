abf51b6094f4b36e1134abd7225a25868c72c99e
Felipe <pipecalgaro@gmail.com>
2022-07-13 18:05:00 -0300

    Without react query

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useFetch } from './hooks/useFetch'
+
+type Repository = {
+  full_name: string
+  description: string
+}
+
+function App() {
+  const { data: repositories, isFetching } = useFetch<Repository[]>('users/felipecalgaro/repos')
+
+  return (
+    <ul>
+      {isFetching && <p>Loading...</p>}
+      {repositories?.map(repo => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default App
